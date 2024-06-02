f7e5119ea90fc091d294486d822bb3cea33b81f6
koalapanda96 <jkuceradc@seznam.cz>
2024-06-02 23:12:57 +0200

    dummy

--- app.js
+++ app.js
@@ -18,8 +18,21 @@ app.use(express.static('src'));
 
 
 app.get('/', async (req, res) => {
-        res.render('site');
-});
+        try {
+            // Fetch data from Instagram Graph API
+            const accessToken = "IGQWRNbndBNFk2R0VNWE1TSGJuVDVaVnFEd29pc1JjVEFNVTdkTjdXQlBiR0hLZAkJmWmQ5MHlDOFlBLXF1WG1UbTB1djhDNTI4dkwybmdyZA0p6R3ZAKbmU2MzQtU2t2LUFFRWdFbUNuZA0taMTFpaTh4NzFXTllnc3MZD";
+            const apiUrl = `https://graph.instagram.com/v12.0/me/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${accessToken}`;
+            const response = await axios.get(apiUrl);
+            const imageData = response.data.data;
+            console.log(response)
+    
+            // Render site.ejs and pass imageData as a local variable
+            res.render('site', { imageData });
+        } catch (error) {
+            console.error('Error fetching Instagram data:', error);
+            res.status(500).send('Error fetching Instagram data');
+        }
+    });
 
 app.listen(port, () => {
     console.log(`App jede na portu ${port}`);
